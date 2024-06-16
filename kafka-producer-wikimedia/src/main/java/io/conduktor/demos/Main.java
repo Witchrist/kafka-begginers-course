ef0f35dc565e12552cd624f5e3c975525a6d76b4
Christopher Alexander <christopherqueiroz@hotmail.com>
2024-06-16 15:38:12 -0300

    Aumento de taxa de mensagens trasmitidas e criacao de projeto consumer opensearch

--- kafka-producer-wikimedia/src/main/java/io/conduktor/demos/Main.java
+++ kafka-producer-wikimedia/src/main/java/io/conduktor/demos/Main.java
@@ -5,7 +5,7 @@ import io.conduktor.demos.kafka.wikimedia.WikimediaChangesProducer;
 public class Main {
     public static void main(String[] args) {
         WikimediaChangesProducer wikimedia = new WikimediaChangesProducer();
-        
+
         try {
             wikimedia.start();
         } catch (InterruptedException e) {
